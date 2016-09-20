f2abb08a34aa886af8d83bf348807cfae9d669da
Mark D'Arensbourg <mdarens@gmail.com>
2016-09-20 10:37:47 -0500

    collapse doesn't work yet, take it out

--- src/alert.jsx
+++ src/alert.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import Collapse from "./collapse";
 
 const Alert = ({ type = "info", message, headline, onDismiss }) => {
 	const css = "alert alert-dismissible alert-" + type;
 	const headlineEl = headline ? <strong>{headline}&nbsp;</strong> : null;
 
 	return (
-		<div>
-			<Collapse>
-				<div style={{ margin: 2 }} className={css} >
-					<button type="button" className="close" title="Dismiss" onClick={onDismiss}>&times;</button>
-					{headlineEl}{message}
-				</div>
-			</Collapse>
+		<div style={{ margin: 2 }} className={css} >
+			<button type="button" className="close" title="Dismiss" onClick={onDismiss}>&times;</button>
+			{headlineEl}{message}
 		</div>
 	);
 };
