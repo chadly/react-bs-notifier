4d5dbf50c8f7f823c8bae3f0ec484a460413bb1e
Mark <mdarens@gmail.com>
2017-01-17 10:20:21 -0600

    split alert styles into own file

---
+++ src/alert.style.js
@@ -0,0 +1,44 @@
+import { bootstrap } from "toetag";
+
+export default {
+	alert: {
+		composes: "alert",
+		marginBottom: bootstrap.paddingBaseVertical,
+		display: "inline-block",
+		textAlign: "left"
+	},
+	info: {
+		composes: "alert-info"
+	},
+	success: {
+		composes: "alert-success"
+	},
+	warning: {
+		composes: "alert-warning",
+	},
+	danger: {
+		composes: "alert-danger"
+	},
+	dismissable: {
+		composes: "alert-dismissable"
+	},
+	close: {
+		composes: "close"
+	},
+	msgContainer: {
+		display: "inline-block"
+	},
+	icon: {
+		verticalAlign: "top",
+		fontSize: bootstrap.fontSizeH4,
+		paddingRight: bootstrap.paddingLargeHorizontal,
+		opacity: 0.2
+	},
+	headline: {
+		margin: 0,
+		marginBottom: bootstrap.paddingBaseVertical
+	},
+	body: {
+		maxWidth: "40em"
+	}
+};
