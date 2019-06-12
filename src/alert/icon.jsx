12a26125b2a3d76fba77b1e2fbc77b8d3d50b0f7
Chris <chris@chris-Ubuntu.(none)>
2019-06-12 12:24:12 -0400

    Added optional support for React Icons

--- src/alert/icon.jsx
+++ src/alert/icon.jsx
@@ -1,14 +1,45 @@
 import React from "react";
 
+
+// Import optional react-icons library
+import("react-icons/fa").then(fa => {
+  const { FaCheck, FaExclamationCircle, FaExclamationTriangle, FaInfo } = fa;
+}).catch(err => {
+  const FaCheck = null;
+});
+
 const Icon = ({ type, className = "" }) => {
 	const faType = iconType(type);
 
 	if (faType) {
-		return <i className={`${faType} ${className}`} aria-hidden="true" />;
+	  if (FaCheck) {
+	    return <ReactIcon type={type} className={className} />
+	  } else {
+  		return <i className={`${faType} ${className}`} aria-hidden="true" />;
+  }
 	}
 
 	return null;
 };
+
+const ReactIcon = ({ type, className = ""}) => {
+  switch (type) {
+    case "warning":
+      return <FaExclamationTriangle className={className} aria-hidden="true" />;
+    
+    case "info":
+      return <FaInfo className={className} aria-hidden="true" />;
+    
+    case "success":
+      return <FaCheck className={className} aria-hidden="true" />;
+    
+    case "danger":
+      return <FaExclamationCircle className={className} aria-hidden="true" />;
+    
+    default:
+      return null;    
+  }
+}
 
 function iconType(type) {
 	switch (type) {
